17c9c8b40ac47545941708ab8fcf3df9b494d27a
Jounts <jounts@gmail.com>
2020-05-23 11:03:29 +0300

    added peek func

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -41,8 +41,10 @@ def peek(ind: int = 0) -> Any:
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
+    global stack_list
+
     print(ind)
-    return None
+    return stack_list[-ind -1] if len(stack_list) > ind else None
 
 
 def clear() -> None:
